a0776b796949b0091c6552b3da924fcb5c8d673f
Nika Nasteski <nikadrob@gmail.com>
2025-01-09 22:56:15 +0100

    project and conact pages

--- ClientApp/src/pages/projects.tsx
+++ ClientApp/src/pages/projects.tsx
@@ -1,10 +1,36 @@
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
-export default function Projects() {
+interface Project {
+  id: number;
+  name: string;
+  description: string;
+  imageUrl: string;
+}
+
+const Projects: React.FC = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    fetch("/api/projects")
+      .then((response) => response.json())
+      .then((data) => setProjects(data))
+      .catch((error) => console.error("Error fetching projects:", error));
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <p>projects</p>
+      <h1>Our Projects</h1>
+      {projects.map((project) => (
+        <div key={project.id}>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+          <img src={project.imageUrl} alt={project.name} />
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+export default Projects;
